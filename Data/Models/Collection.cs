907a94f31b22ccab08adee504ee1e776e68e29af
Ensar <ensarhamzic01@gmail.com>
2022-09-09 17:35:07 +0200

    Collection Model

---
+++ Data/Models/Collection.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies_API.Data.Models
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
