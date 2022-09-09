83ae83df25668e71123aca4cdeca9c8ea3d9cfde
Ensar <ensarhamzic01@gmail.com>
2022-09-09 17:34:56 +0200

    login/register

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movies_API.Data.Services;
+using Movies_API.Data.ViewModels;
+
+namespace Movies_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private UserService userService;
+
+        public UsersController(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult RegisterUser([FromBody] UserRegisterVM request)
+        {
+            try
+            {
+                var NewUser = userService.RegisterUser(request);
+                return Created(nameof(NewUser), NewUser);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult LoginUser([FromBody] UserLoginVM request)
+        {
+            try
+            {
+                var user = userService.LoginUser(request);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}
